e332066e53d2ace3ecc25a28dbd6bf010d05f256
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-05-19 03:07:46 +0530

    added a main-header component;

---
+++ foodies-app/components/main-header.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
+
+export default function MainHeader() {
+  return (
+    <>
+      <header className={classes.header}>
+        <Link href='/' className={classes.logo}>
+          <img src={logoImg.src} alt='A plate with food on it' />
+          NextLevel Foodie
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href='/meals'>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href='/community'>Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
