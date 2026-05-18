221ded351ebe26833893e9ffb220be8e495066fa
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-05-19 04:56:06 +0530

    added error, not-found components;

---
+++ foodies-app/app/not-found.jsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <>
+      <main className='not-found'>
+        <h1>Not Found</h1>
+        <p>These are not the meals you are looking for!</p>
+      </main>
+    </>
+  );
+}
