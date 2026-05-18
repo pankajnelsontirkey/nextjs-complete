e6607a50401def812de32c4202c9e91a65e01295
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-05-19 04:39:14 +0530

    added a method for fetching meals data; added meals data to MealsPage;

--- foodies-app/app/meals/page.jsx
+++ foodies-app/app/meals/page.jsx
@@ -1,8 +1,11 @@
 import Link from 'next/link';
-import classes from './page.module.css';
-import MealsGrid from '@/components/meals/meals-grid';
 
-export default function MealsPage() {
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+import classes from './page.module.css';
+
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -16,7 +19,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
--- foodies-app/components/meals/meals-grid.jsx
+++ foodies-app/components/meals/meals-grid.jsx
@@ -1,10 +1,10 @@
 import MealItem from './meal-item';
-import classes from './meals.grid.module.css';
+import classes from './meals-grid.module.css';
 
 export default function MealsGrid({ meals }) {
   return (
     <>
-      <ul className={classes.meal}>
+      <ul className={classes.meals}>
         {meals.map((meal) => (
           <li key={meal.id}>
             <MealItem {...meal} />
