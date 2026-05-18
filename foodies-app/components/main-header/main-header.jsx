7487b7f08c259755f9901c86d9cae83de2fba05b
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-05-19 03:22:44 +0530

    added a main-header-background component;

--- foodies-app/components/main-header/main-header.jsx
+++ foodies-app/components/main-header/main-header.jsx
@@ -1,14 +1,17 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import logoImg from '@/assets/logo.png';
+import MainHeaderBackground from './main-header-background';
 import classes from './main-header.module.css';
 
 export default function MainHeader() {
   return (
     <>
+      <MainHeaderBackground />
       <header className={classes.header}>
         <Link href='/' className={classes.logo}>
-          <img src={logoImg.src} alt='A plate with food on it' />
+          <Image src={logoImg} alt='A plate with food on it' priority />
           NextLevel Foodie
         </Link>
 
