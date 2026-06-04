e38155b82fbc312eeff993c92c6025c2682e912a
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-06-04 09:40:59 +0530

    moved shareMeal method from ShareMealPage to server action; created saveMeal method;

--- foodies-app/lib/meals.js
+++ foodies-app/lib/meals.js
@@ -1,4 +1,8 @@
+import fs from 'node:fs';
+
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
 
 const db = sql('meals.db');
 
@@ -9,6 +13,42 @@ export async function getMeals() {
 
   return db.prepare('SELECT * FROM meals').all();
 }
+
 export /* async */ function getMeal(slug) {
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split('.').pop();
+  const fileName = `${meal.slug}_${Date.now()}.${extension}`;
+
+  const writer = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  writer.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error('Error while saving image!');
+    }
+  });
+
+  meal.image = `/images/${fileName}`;
+
+  db.prepare(
+    `
+    INSERT INTO meals
+      (title, summary, instructions, creator, creator_email, image, slug)
+    VALUES(
+      @title,
+      @summary,
+      @instructions,
+      @creator, 
+      @creator_email,
+      @image,
+      @slug
+      )
+    `
+  ).run(meal);
 }
