aaa84c1c1ed983ff776c0e82a8f44b1962b45558
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-09-04 04:08:49 +0530

    created /archive path with parallel routes @archive & @latest; added default page to @latest; added catch-all [[...filter]] route segment to @archive; added year, month navigation to @archive page;

---
+++ routing-and-rendering/app/archive/@archive/[[...filter]]/page.jsx
@@ -0,0 +1,58 @@
+import Link from 'next/link';
+
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth
+} from '@/app/lib/news';
+import NewsList from '@/components/news-list';
+
+export default async function FilteredNewsPage({ params }) {
+  const { filter } = await params;
+
+  const year = filter?.[0];
+  const month = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (year && !month) {
+    news = getNewsForYear(year);
+    links = getAvailableNewsMonths(year);
+  }
+
+  if (year && month) {
+    news = getNewsForYearAndMonth(year, month);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for selected filter.</p>;
+
+  if (news?.length) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  return (
+    <>
+      <header id='archive-header'>
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = year
+                ? `/archive/${year}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <Link key={link} href={href}>
+                  {link}
+                </Link>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+}
