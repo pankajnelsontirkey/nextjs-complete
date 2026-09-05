73dc529b4451155275da47324f5219d782749955
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-09-06 00:07:36 +0530

    created ModalBackdrop component;created a NewsDetailsLayout with modal and children as params; created a @modal parallel route with intercepted page for article image, also added default page for @modal route;

---
+++ routing-and-rendering/app/news/[slug]/@modal/(.)image/page.jsx
@@ -0,0 +1,24 @@
+import { notFound } from 'next/navigation';
+
+import ModalBackdrop from '@/components/modal-backdrop';
+import { DUMMY_NEWS } from '@/dummy-news';
+
+export default async function InterceptedImagePage({ params }) {
+  const { slug } = await params;
+  const newsItem = DUMMY_NEWS.find((item) => item.slug === slug);
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <>
+      <ModalBackdrop />
+      <dialog className='modal' open>
+        <div className='fullscreen-image'>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </div>
+      </dialog>
+    </>
+  );
+}
